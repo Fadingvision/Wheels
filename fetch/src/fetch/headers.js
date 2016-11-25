32b287c5a06ba162fcfb7cf507a6414cca7ce2c2
blackLearning <fuyigekua@foxmail.com>
2016-11-25 09:00:51 +0800

    append method

--- fetch/src/fetch/headers.js
+++ fetch/src/fetch/headers.js
@@ -4,9 +4,27 @@ export default class Headers {
 		this.map = new Map();
 	}
 
-	append() {}
+	append(key, value) {
+		let oldValue = this.get(key);
+		let newValue;
+		if(this.has(key)) {
+			if( Array.isArray(oldValue) ){
+				newValue = oldValue;
+				newValue.push(value);
+			} else {
+				newValue = [];
+				newValue.push(oldValue);
+				newValue.push(value);
+			}
+			this.set(key, newValue);
+		}else {
+			this.set(key, value);
+		}
+	}
 
-	delete() {}
+	delete(key) {
+		return this.map.delete(key);
+	}
 
 	entries() {}
 
