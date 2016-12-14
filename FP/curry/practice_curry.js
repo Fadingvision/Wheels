376bf4fa260fe5e1ff5a450b87c8ea3852b426e9
blackLearning <fuyigekua@foxmail.com>
2016-12-15 01:17:32 +0800

    add exam

--- FP/curry/practice_curry.js
+++ FP/curry/practice_curry.js
@@ -1,9 +1,64 @@
 const _ = require('lodash');
 
 
+/** basic usage */
 function add(a, b) {
     return a + b;
 
 }
 let curried_add = _.curry(add);
 console.log(curried_add(1)(2));
+
+// 只传给函数一部分参数通常也叫做局部调用（partial application），能够大量减少样板文件代码（boilerplate code）。
+
+// 练习 1
+//==============
+// 通过局部调用（partial apply）移除所有参数
+
+var words = function(str) {
+  return split(' ', str);
+};
+
+// 练习 1a
+//==============
+// 使用 `map` 创建一个新的 `words` 函数，使之能够操作字符串数组
+
+var sentences = undefined;
+
+
+// 练习 2
+//==============
+// 通过局部调用（partial apply）移除所有参数
+
+var filterQs = function(xs) {
+  return filter(function(x){ return match(/q/i, x);  }, xs);
+};
+
+
+// 练习 3
+//==============
+// 使用帮助函数 `_keepHighest` 重构 `max` 使之成为 curry 函数
+
+// 无须改动:
+var _keepHighest = function(x,y){ return x >= y ? x : y; };
+
+// 重构这段代码:
+var max = function(xs) {
+  return reduce(function(acc, x){
+    return _keepHighest(acc, x);
+  }, -Infinity, xs);
+};
+
+
+// 彩蛋 1:
+// ============
+// wrap array's slice to be functional and curried.
+// 包裹数组的 `slice` 函数使之成为 curry 函数
+// //[1,2,3].slice(0, 2)
+var slice = undefined;
+
+
+// 彩蛋 2:
+// ============
+// 借助 `slice` 定义一个 `take` curry 函数，接受 n 个元素为参数。
+var take = undefined;
