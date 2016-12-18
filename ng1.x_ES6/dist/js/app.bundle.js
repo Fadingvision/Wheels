a9d52a4236e4031a84a6d75f13f85a7e63a60017
blackLearning <fuyigekua@foxmail.com>
2016-12-18 23:18:55 +0800

    rm useless files

--- ng1.x_ES6/dist/js/app.bundle.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([0,1],[function(n,exports,o){!function(){throw new Error('Cannot find module "babel-polyfill"')}(),function(){throw new Error('Cannot find module "/home/cxy/other_stuff/github/Wheels/ng1.x_ES6/client/app.js"')}()}]);
