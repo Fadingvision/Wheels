4d6d402eeb70d0d8c7fa0db9102ede9e268e836e
blackLearning <fuyigekua@foxmail.com>
2019-03-24 22:38:40 +0800

    Update examples

--- websocketServer/examples/index.ts
+++ websocketServer/examples/index.ts
@@ -1,14 +1,38 @@
-import WsClient from '../src/client';
-import WebSocketServer from '../src/index';
+import * as socketIO from "socket.io";
 
-const wss = new WebSocketServer({
-  port: 8888,
-});
+function socketServer() {
+  const wsServer = socketIO();
+  wsServer.listen(9001);
+  // wsServer.on("connection", clientSocket => {
+  //   clientSocket.on("PING", msg => {
+  //     // console.log('Received PING from ', clientSocket.handshake.headers);
+  //     let now = new Date();
+  //     // PONG is the event name
+  //     clientSocket.emit("PONG", `PONG ${new Date()}`);
+  //   });
+  // }); //end of on Client connection event
+}
+socketServer();
 
-wss.on('connection', function connection(ws: WsClient) {
-  ws.on('message', function incoming(message: string) {
-    // Broadcast to everyone else.
-    console.log(message);
-  });
-  // ws.send('something');
-});
+
+// import WsClient from '../src/client';
+// import WebSocketServer from '../src/index';
+
+// import * as webSocket from "ws";
+// const WebSocketServer = webSocket.Server;
+
+// const wss = new WebSocketServer({
+//   port: 9001
+// });
+
+// interface IMessage {
+//   a: string;
+// }
+
+// wss.on('connection', function connection(ws: WsClient) {
+//   ws.on('message', function incoming(message: string) {
+//     // Broadcast to everyone else.
+//     console.log(JSON.parse(message));
+//   });
+//   // ws.send('something');
+// });
